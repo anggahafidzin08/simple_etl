763a9d85d29c1e282950ccc213db888ee1115586
Angga Putra Hafidzin <angga.hafidzin@bellanina.com>
2024-08-10 09:04:48 +0700

    update main.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import subprocess
 def connect_db(creds, sub_data):
     with open(creds) as f:
         data = json.load(f)[sub_data]
-    con = pg.connect("dbname={a} user={b} password={c} host={d} port={e}".format(a=data['DB_NAME'], b=data['DB_USER'], c=data['DB_PASSWORD']
-                                                                             , d=data['DB_HOST'], e=data['DB_PORT']))
+    con = pg.connect("dbname={a} user={b} password={c} host={d} port={e}".format(a=data['DB_NAME'], b=data['DB_USER']
+                                , c=data['DB_PASSWORD'], d=data['DB_HOST'], e=data['DB_PORT']))
     cur = con.cursor()
     return con, cur;
--- main.py
+++ main.py
@@ -6,18 +6,18 @@ import os
 import sqlparse
 
 if __name__ == "__main__":
-    # declare file path
-    config_file = "C:\\Users\\angga.hafidzin\\Data Business Intelligence\\VPN Documents\\configla-prod.ovpn"
-    credentials_file = "C:\\Users\\angga.hafidzin\\Data Business Intelligence\\Analysis\\Python Scripts\\B2B Dashboard Updates Documents\\db_creds.json"
-    
-    # connect to OpenVPN
-    config.connect_vpn(config_file, credentials_file)
-    
     # connect to database
-#     con, cur = config.connect_db('db_creds.json', 'database')
+    con, cur = config.connect_db('credentials.json', 'marketplace_prod')
     
-#     # mengambil sql query
-#     path_query = os.getcwd()
-#     query_stock = sqlparse.format(open(path_query + '\\stock_warehouse.sql', 'r').read(), strip_comments=True).strip()
-#     stock = pd.read_sql(query_stock, con)
-#     print(stock.head())
+    # mengambil sql query
+    # path_query = os.getcwd()
+    # query_stock = sqlparse.format(open(path_query + '\\stock_warehouse.sql', 'r').read(), strip_comments=True).strip()
+    # stock = pd.read_sql(query_stock, con)
+    # print(stock.head())
+    print(pd.read_sql_query("""
+        select *
+        from marketplace.tb_orders
+        order by 1
+    """, con))
+    con.close()
+    cur.close()
